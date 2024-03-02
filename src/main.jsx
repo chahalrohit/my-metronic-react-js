47c8b72d26f62559e56e084f5848a9b11e0ecd16
chahalrohit <rcrchahal@gmail.com>
2024-03-02 21:34:00 +0530

    initial commit

---
+++ src/main.jsx
@@ -0,0 +1,47 @@
+import React from "react";
+import ReactDOM from "react-dom/client";
+import { RouterProvider, createBrowserRouter } from "react-router-dom";
+import App from "./App.jsx";
+import About from "./components/About/About.jsx";
+import Contact from "./components/Contact/Contact.jsx";
+import Home from "./components/Home/Home.jsx";
+import "./index.css";
+
+const router = createBrowserRouter([
+  {
+    path: "/",
+    element: <App />,
+    children: [
+      {
+        path: "",
+        element: <Home />,
+      },
+      {
+        path: "about",
+        element: <About />,
+      },
+      {
+        path: "contact",
+        element: <Contact />,
+      },
+    ],
+  },
+]);
+
+// const router = createBrowserRouter(
+//   createRoutesFromElements(
+//     <Route path="/" element={<Layout />}>
+//       <Route path="" element={<Home />} />
+//       <Route path="about" element={<About />} />
+//       <Route path="contact" element={<Contact />} />
+//       <Route path="user/:userid" element={<User />} />
+//       <Route loader={githubInfoLoader} path="github" element={<Github />} />
+//     </Route>
+//   )
+// );
+
+ReactDOM.createRoot(document.getElementById("root")).render(
+  <React.StrictMode>
+    <RouterProvider router={router} />
+  </React.StrictMode>
+);
